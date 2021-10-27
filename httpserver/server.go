f37375b205c6669de261036243d4f6a63557d0fb
chase <chasemalbright33@gmail.com>
2021-10-26 23:42:03 -0500

    http server creation - listening on port 8080

---
+++ httpserver/server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world!")
+	})
+
+	fmt.Printf("Starting a server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
